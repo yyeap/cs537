61422d62ac91a3be6e6fb7e20e9c4812f3f2e932
Lee <Lee.Yerkes@gmail.com>
2012-11-26 21:52:25 -0600

    pushing update to input

---
+++ a3/input.h
@@ -0,0 +1,28 @@
+/*
+Yuen Lye Yeap
+Lee Yerkes
+cs537 Project 3
+
+input.h
+
+*/
+#include <string.h>
+
+/*
+parseTrace
+
+this function parses one line of a trace file
+
+in:
+char* line - a line from the trace file to be parsed
+
+out:
+char* name - the name of job
+int* start_time - start time of job
+int* cpu_time - cpu time of job
+int* io_count - number of bytes of IO done by job
+
+return:
+none
+ */
+void parseTrace(char* line, char* name, int* start_time, int* cpu_time, int* io_count);
