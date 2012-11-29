d693ec5ba12b123a7dfd6d9a5eb6169761d4dc30
Lee <Lee.Yerkes@gmail.com>
2012-11-28 18:38:00 -0600

    updated main file

--- a3/537sim.c
+++ a3/537sim.c
@@ -9,6 +9,7 @@ Contains implementation of simulator main loop and stuff.
 
 /*INITIALIZATION
 
+Process* current_process = NULL;
 stats* stats;
 init_stats(stats);
 queue* q;
@@ -18,15 +19,33 @@ int io;
 int ts; 
 int ar;
 */
+int main (int argc, char* argv[]){
+  if (2 != argc){
+    printf("ERROR: Wrong number of parameter. Need one argument.");
+    return -1;
+  }
+  while(!done) {
+    if (NULL != current_process) {
+      clock++;
+    }
+    
+    io = get_IO_Complete();
+    ts = get_timeslice(q);
+    ar = get_arrival();
+    
+    clock += min(io, ts, ar);
+    
+    if(io <= ts && io <= ar) {
+      clock += io;
+    }
+    else if (ts <= io && ts <= ar) {
+      clock += ts;
+    } 
+    else {
+      clock += ar;
+      current_process;
+    }
+  }
 
-/* BEGIN LOOP
-
-io = get_IO_Complete();
-ts = get_timeslice(q);
-ar = get_arrival();
-
-clock += min(io, ts, ar);
-
-
-
-END LOOP */
+  return 0;
+}
