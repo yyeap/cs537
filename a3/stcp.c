e00a71e561706833dd0aba975e688f79e3751180
Lee <Lee.Yerkes@gmail.com>
2012-12-02 19:30:29 -0600

    ditched the rb tree

--- a3/stcp.c
+++ a3/stcp.c
@@ -5,27 +5,31 @@ stcp.c
 
 //Process cpu time comparison for rb tree
 int pCompare(const void* a, const void* b){
-  if(a->cpu > b->cpu) return 1;
-  if(a->cpu < b->cpu) return -1;
+  if(a->cpu < b->cpu) return 1;
   return 0;
 } 
 
 //insert into rb tree with priority CPU time
 void add_process(process* new_p, void* q) {
-  RBTreeInsert((rb_red_blk_tree*)q, new_p, 0);
+  pq_enqueue((pq_t)q, (pq_data_t)new_p, NULL);
 }
 
 process* get_process(void* q) {
   // examine smallest element and return process pointer
-  rb_red_blk_node* newNode;
-  
+  process* p;
+  p = (process*)pq_dequeue((pq_t)q, NULL);
+  return p;
 }
 
 int get_timeslice(int time, void* q) {
   // return time + CPU time of shortest process
+  process* p;
+  p = (process*)pq_inspect_next((pq_t)q, NULL);
+  return p->cpu_remaining + time;
 }
 
 void init_q(void* q) {
-  // initialize rb tree and assign to pointer 
-  q = (void*)RBTreeCreate(pCompare, NullFunction, NullFunction, NullFunction, NullFunction);
+  // initialize priority queue
+  pq_status_t status;
+  q = (void*)pq_new_queue(0, pCompare, status);
 }
